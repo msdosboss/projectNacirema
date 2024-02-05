2435bbca456a87ef5868ab96f0d489121550bb32
msdosboss <jacobt0722@gmail.com>
2024-02-05 09:16:44 -0800

    Fixed typo in winClient.c

--- winClient.c
+++ winClient.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]){
     char *port;
     strcpy(port, defaultPort);
     if(argc > 1){
-        if((port = inputPortPort(argc, argv)) == NULL){
+        if((port = inputPort(argc, argv)) == NULL){
             return -1;
         }
     }
