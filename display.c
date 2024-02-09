58120717be90c787d6206b3f56520a84aaaa8626
orcalord <jacobt0722@gmail.com>
2024-02-08 16:47:12 -0800

    display.c renders a grid of squares

--- display.c
+++ display.c
@@ -5,8 +5,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL.h>
+#define COLLUMNS 10
+#define ROWS 10
 #define WIDTH 640
-#define HEIGHT 480
+#define HEIGHT 640
 #define SIZE 200
 #define FPS 60
 
@@ -42,6 +44,14 @@ int main(int argc, char* argv[]){
   /* Main loop */
   bool running = true, left_pressed = false, right_pressed = false;
   float x_pos = (WIDTH-SIZE)/2, y_pos = (HEIGHT-SIZE)/2, x_change = 0, y_change = 0;
+  SDL_Rect **rects;
+  rects = malloc(sizeof(SDL_Rect *) * COLLUMNS);
+  for(int i = 0; i < COLLUMNS; i++){
+    rects[i] = malloc(sizeof(SDL_Rect) * ROWS);
+    for(int j = 0; j < ROWS; j++){
+	  rects[i][j] = (SDL_Rect){i * 64, j * 64, 64, 64};
+    }
+  } 
   SDL_Rect rect = {(int) x_pos, (int) y_pos, SIZE, SIZE};
   SDL_Event event;
   while (running)
@@ -51,7 +61,7 @@ int main(int argc, char* argv[]){
     {
       switch (event.type)
       {
-        case SDL_QUIT:
+    	case SDL_QUIT:
           running = false;
           break;
         case SDL_KEYDOWN:
@@ -97,16 +107,29 @@ int main(int argc, char* argv[]){
     x_pos += x_change;
     y_pos += y_change;
     if (x_pos <= 0)
-      x_pos = 0;
+    	x_pos = 0;
     if (x_pos >= WIDTH - rect.w)
-      x_pos = WIDTH - rect.w;
+    	x_pos = WIDTH - rect.w;
     if (y_pos <= 0)
-      y_pos = 0;
+    	y_pos = 0;
     rect.x = (int) x_pos;
     rect.y = (int) y_pos;
     /* Draw the rectangle */
-    SDL_SetRenderDrawColor(rend, 255, 0, 255, 127);
-    SDL_RenderFillRect(rend, &rect);
+    //SDL_SetRenderDrawColor(rend, 255, 0, 255, 127);
+    //SDL_RenderFillRect(rend, &rect);
+    for(int i = 0; i < COLLUMNS; i++){
+		fflush(stdout);
+    	for(int j = 0; j < ROWS; j++){
+    		if(((j + 1) % 2 != 0 && (i + 1) % 2 != 0) || ((j + 1) % 2 == 0 && (i + 1) % 2 == 0)){
+				SDL_SetRenderDrawColor(rend, 255, 0, 255, 127);
+			}
+			else{
+				SDL_SetRenderDrawColor(rend, 255, 0, 0, 127);
+			}
+    		SDL_RenderFillRect(rend, &rects[i][j]);
+			//SDL_RenderPresent(rend);
+      	}
+    } 
     /* Draw to window and loop */
     SDL_RenderPresent(rend);
     SDL_Delay(1000/FPS);
